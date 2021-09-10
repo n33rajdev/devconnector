e9d0a5ffdb93adb196a4c62f4dedd7d70375bc0a
n33rajdev <n33rajdev@gmail.com>
2021-09-10 21:43:34 +0530

    Added user validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
-
+const { body, validationResult } = require('express-validator/check');
 
 // @route POST api/user
 // @desc  Register user
 // @access Public
-router.post('', (req, rsp) => {
+router.post(
+  '',
+  [
+    body('name', 'name is required').not().isEmpty(),
+    body('email', 'provide email').isEmail(),
+    body('password', 'Provide password').isLength({ min: 6 }),
+  ],
+  (req, res) => {
     console.log(req.body);
-    rsp.send('User Route');
-});
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('User Route');
+  }
+);
 
 module.exports = router;
